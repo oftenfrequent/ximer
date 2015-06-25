d6eb647a43c710969198fab7223ccffc38c461d4
k23Burke <k23burke@gmail.com>
2015-06-25 16:05:43 -0400

    initial commit

---
+++ server/app/routes/index.js
@@ -0,0 +1,30 @@
+'use strict';
+var router = require('express').Router();
+var fs = require('fs');
+var path = require('path');
+module.exports = router;
+
+router.use('/', function (req, res, next) {
+	console.log('HERE');
+	next();
+})
+
+router.use('/wav/:loopname', function (req, res, next) {
+	var p = path.join(__dirname, '../../../wav/');
+	console.log('LOOP NAME', p);
+	fs.readFile(p+req.params.loopname, function (err, data) {
+		if(err) console.log(err);
+		else {
+			res.send(data);
+		}
+	})
+});
+
+router.use('/tutorial', require('./tutorial'));
+router.use('/members', require('./members'));
+
+// Make sure this is after all of
+// the registered routes!
+router.use(function (req, res) {
+    res.status(404).end();
+});
