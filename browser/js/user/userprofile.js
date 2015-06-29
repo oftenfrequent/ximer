772f3e4dc90677efd5ed34d3c2aecfbbe2f8442d
Jovi Chokor <jomonavi@gmail.com>
2015-06-29 18:34:33 -0400

    finished userprofile

--- browser/js/user/userprofile.js
+++ browser/js/user/userprofile.js
@@ -1,13 +1,9 @@
-app.config(function ($stateProvider) {
+app.config(function($stateProvider) {
 
     $stateProvider.state('membersOnly', {
-        url: '/members-area',
-        template: '<img ng-repeat="item in stash" width="300" ng-src="{{ item }}" />',
-        controller: function ($scope, SecretStash) {
-            SecretStash.getStash().then(function (stash) {
-                $scope.stash = stash;
-            });
-        },
+        url: '/userprofile',
+        templateUrl: 'js/user/userprofile.html',
+        controller: 'UserController',
         // The following data.authenticate is read by an event listener
         // that controls access to this state. Refer to app.js.
         data: {
@@ -17,16 +13,8 @@ app.config(function ($stateProvider) {
 
 });
 
-app.factory('SecretStash', function ($http) {
+app.controller('UserController', function($scope, $state, AuthService) {
 
-    var getStash = function () {
-        return $http.get('/api/members/secret-stash').then(function (response) {
-            return response.data;
-        });
-    };
 
-    return {
-        getStash: getStash
-    };
 
 });
